5ae963958c2c7110cfc74cc89e54e63aa6a2bcce
williamhCode <83525937+williamhCode@users.noreply.github.com>
2021-10-11 10:34:13 +0800

    .

--- Lesson 1/calculator.py
+++ Lesson 1/calculator.py
@@ -11,15 +11,15 @@ def division(n1, n2):
     return n1/n2
 
 while True:
-    n1 = float(input('Enter First Number: '))
-    n2 = float(input('Enter Second Number: '))
+    n1 = int(input('Enter First Number: '))
+    n2 = int(input('Enter Second Number: '))
     type = input('Enter Operation: ')
-    if type == 'a':
+    if type == '+':
         print(addition(n1, n2))
-    elif type == 's':
+    elif type == '-':
         print(subtraction(n1, n2))
-    elif type == 'm':
+    elif type == '*':
         print(multiplication(n1, n2))
-    elif type == 'd':
+    elif type == '/':
         print(division(n1, n2))
     print()
