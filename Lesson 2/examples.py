4225a0563151a33186ec732cf2a8af18780cdc7b
williamhCode <83525937+williamhCode@users.noreply.github.com>
2021-11-07 21:28:21 +0800

    .

---
+++ Lesson 2/examples.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+# ### Lesson 2
+# 
+# - AI News
+# - AI Applications
+
+# %% [markdown]
+# Let's create a simple calculator using Github Copilot.
+# 
+# Here are the instructions:
+# ```python
+# # write a calculator that can add, subtract, multiply and divide
+# # get two numbers from the user and perform an operation
+# ```
